4332ff7e96ca934daf42c86a4d017c64d608ad6c
Luiz Alberto Huller da Silva <95250997+LuizAlbertoDev@users.noreply.github.com>
2026-05-24 23:34:58 -0300

    Praticando JS
    Praticando JS com Notas de alunos usando o ForEach

---
+++ Desafios_JS/Desafio_081.js
@@ -0,0 +1,7 @@
+const alunos = [
+    {nome:"Luiz", nota:8},
+    {nome:"Ana",nota:9},
+    {nome:"Carlos",nota:7},
+]
+
+alunos.forEach(aluno => console.log(`Aluno: ${aluno.nome} - Nota: ${aluno.nota}`))
