f1924e73db4122857666fea10ba6c0642504415f
Luiz Alberto Huller da Silva <95250997+LuizAlbertoDev@users.noreply.github.com>
2026-05-24 23:11:24 -0300

    Treinando JS
    Fazendo um Array com frutas

---
+++ Desafios_JS/Desafio_078.js
@@ -0,0 +1,8 @@
+const frutas = ["Maçã","Banana","Goiaba","Abacaxi","laranja"]
+
+console.log(`Primeira Fruta:${frutas[0]}, Ultima Fruta:${frutas[frutas.length-1]}`)
+console.log(`Total de Frutas :${frutas.length}`)
+
+frutas.push("Melancia")
+
+console.log(frutas)
