ee5d420baf1a528112f8a05141c011aa6059248b
Luiz Alberto Huller da Silva <95250997+LuizAlbertoDev@users.noreply.github.com>
2026-06-15 16:39:50 -0300

    feat: desafio 145 arrow functions

---
+++ Desafios_JS/Desafio_145_arrow_functions.js
@@ -0,0 +1,10 @@
+const somar = (a, b) => a + b;
+
+const cumprimentar = (nome) => `Olá, ${nome}!`
+
+const quadrado = (n) => n * n
+
+
+console.log(somar(3,5))
+console.log(cumprimentar("Luiz"))
+console.log(quadrado(4))
